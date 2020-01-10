820da60e306b5c1c83d4ee1e3c63ea0133ac83f4
Isaac Van Mechelen <vanmeciv@gmail.com>
2020-01-09 20:23:18 -0800

    map is loaded

--- lab-1/js/javascript.js
+++ lab-1/js/javascript.js
@@ -1,1 +1,7 @@
+var map = L.map('map').fitWorld();
 
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaXNhYWN2IiwiYSI6ImNrMnpqYnVxaTA1b3IzbXBnaG5zY3o3eTEifQ.kMdIcXYBFKHTorj3Hxgi7g', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v10',
+}).addTo(map);

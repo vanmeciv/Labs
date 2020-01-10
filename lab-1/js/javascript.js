b73e5f0b132de25e2bb728d5a0f7917f75b70b09
Isaac Van Mechelen <vanmeciv@gmail.com>
2020-01-09 20:30:13 -0800

    added a couple parts of step 2

--- lab-1/js/javascript.js
+++ lab-1/js/javascript.js
@@ -1,7 +1,13 @@
+//required code to add map
 var map = L.map('map').fitWorld();
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaXNhYWN2IiwiYSI6ImNrMnpqYnVxaTA1b3IzbXBnaG5zY3o3eTEifQ.kMdIcXYBFKHTorj3Hxgi7g', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v10',
+    //retina screen detection errors and tile size adjustments
+    tileSize: 512,
+    zoomOffset: -1,
 }).addTo(map);
+//recenters the map on the user's location (browser dialogue box requesting location permissions)
+map.locate({setView: true, maxZoom: 16});

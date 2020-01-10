fdc8d376d82b8c4adc77c11d650e88b91ef698a9
Isaac V <vanmeciv@gmail.com>
2020-01-10 08:13:24 -0800

    Step 2 Complete

--- lab-1/js/javascript.js
+++ lab-1/js/javascript.js
@@ -1,3 +1,4 @@
+// instructions for building this .js found here: https://github.com/UWTMGIS/TGIS_504-Wi20/blob/master/lab-1/Instructions.md
 //required code to add map
 var map = L.map('map').fitWorld();
 
@@ -9,5 +10,18 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     tileSize: 512,
     zoomOffset: -1,
 }).addTo(map);
+//Geolocation
+function onLocationFound(e) {
+    var radius = e.accuracy; //this defines a variable radius as the accuracy value returned by the locate method divided by 2. It is divided by 2 because the accuracy value is the sum of the estimated accuracy of the latitude plus the estimated accuracy of the longitude. The unit is meters.
+    L.marker(e.latlng).addTo(map)  //this adds a marker at the lat and long returned by the locate function.
+        .bindPopup("You are within " + Math.round(radius * 3.28084) + " feet from this point").openPopup(); //this binds a popup to the marker. The text of the popup is defined here as well. Note that we multiply the radius by 3.28084 to convert the radius from meters to feet and that we use Math.round to round the conversion to the nearest whole number.
+    L.circle(e.latlng, radius).addTo(map); //this adds a circle to the map centered at the lat and long returned by the locate function. Its radius is set to the var radius defined above.
+}
+map.on('locationfound', onLocationFound); //this is the event listener
+//popup that runs if location denies access or other error
+function onLocationError(e) {
+  alert(e.message);
+}
+map.on('locationerror', onLocationError);
 //recenters the map on the user's location (browser dialogue box requesting location permissions)
 map.locate({setView: true, maxZoom: 16});

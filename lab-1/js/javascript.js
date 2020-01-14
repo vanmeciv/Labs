23d739bf8bbe114422de08a401c0194571f22e51
Isaac Van Mechelen <vanmeciv@gmail.com>
2020-01-14 15:06:44 -0800

    added notes and comments

--- lab-1/js/javascript.js
+++ lab-1/js/javascript.js
@@ -1,7 +1,6 @@
 // instructions for building this .js found here: https://github.com/UWTMGIS/TGIS_504-Wi20/blob/master/lab-1/Instructions.md
-//required code to add map
+//Lab 1 Step 1: Prepare the page and initialize the map
 // var map = L.map('map').fitWorld(); //commented out for step 4
-
 // L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaXNhYWN2IiwiYSI6ImNrMnpqYnVxaTA1b3IzbXBnaG5zY3o3eTEifQ.kMdIcXYBFKHTorj3Hxgi7g', {
 //     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 //     maxZoom: 18,
@@ -26,7 +25,7 @@ var dark = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?
     tileSize: 512,
     zoomOffset: -1,
 });
-var map = L.map('map', {layers:[light]}).fitWorld();
+var map = L.map('map', {layers:[light]}).fitWorld(); //moved the map initialization to bottom of environmental contions
 //Lab 1 Step 2: Geolocation
 function onLocationFound(e) {
     var radius = e.accuracy; //this defines a variable radius as the accuracy value returned by the locate method divided by 2. It is divided by 2 because the accuracy value is the sum of the estimated accuracy of the latitude plus the estimated accuracy of the longitude. The unit is meters.
@@ -35,6 +34,7 @@ function onLocationFound(e) {
     // L.circle(e.latlng, radius).addTo(map); //this adds a circle to the map centered at the lat and long returned by the locate function. Its radius is set to the var radius defined above.
     // Conditional circle around location based on accuracy (syntax correction "[color" should be "{color"
     // circle only shows on desktop browser if less than certain distance
+// Lab 1 Step 3: Giving the user feedback about the geolocation accuracy of their device
     if (radius <= 100) {
       L.circle(e.latlng, radius, {color: 'green'}).addTo(map);
     }
@@ -58,6 +58,7 @@ function onLocationFound(e) {
       map.addLayer(dark);
     }
 }
+//Lab 1 Step 2: Geolocation
 map.on('locationfound', onLocationFound); //this is the event listener
 //popup that runs if location denies access or other error
 function onLocationError(e) {

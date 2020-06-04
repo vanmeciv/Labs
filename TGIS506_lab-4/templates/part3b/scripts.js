517bc1b10765787bbe37e2d48d82db72a3ca0fdc
Isaac Van Mechelen <vanmeciv@gmail.com>
2020-06-04 13:00:13 -0700

    added title text to bottom of charb 3b and extimated revenue for 2019/2020 seasons

--- TGIS506_lab-4/templates/part3b/scripts.js
+++ TGIS506_lab-4/templates/part3b/scripts.js
@@ -9,10 +9,10 @@ window.onload = function() {
   // labels along the x-axis
   var years = ["2001-02","2002-03","2003-04","2004-05 (NHL Lockout)","2005-06","2006-07","2007-08","2008-09","2009-10","2010-11","2011-12 (NBA Lockout)","2012-13 (NHL Lockout)","2013-14","2014-15","2015-16","2016-17","2017-18","2018-19","2019-2020 Estimates"];
   // For drawing the lines
-  var nflRev = [4.28,4.94,5.33,6.03,6.16,6.54,7.09,7.57,8.02,8.35,8.82,9.17,9.58,11.09,12.16,13.16,13.68,14.48,17];
-  var nbaRev = [2.66,2.72,2.93,3.19,3.37,3.57,3.77,3.79,3.81,3.96,3.68,4.56,4.79,5.18,5.87,7.37,8.01,8.76, ];
-  var nhlRev = [1.875,1.996,2.083,,2.178,2.44,2.75,2.82,2.93,3.09,3.37,2.63,3.7,3.98,4.1,4.43,4.86,5.09, ];
-  var mlbRev = [3.62,3.88,4.27,4.73,5.11,5.48,5.82,5.9,6.14,6.36,6.81,7.1,7.86,8.39,9.03,9.46,9.9,10.37, ];
+  var nflRev = [4.28,4.94,5.33,6.03,6.16,6.54,7.09,7.57,8.02,8.35,8.82,9.17,9.58,11.09,12.16,13.16,13.68,14.48,16.75];
+  var nbaRev = [2.66,2.72,2.93,3.19,3.37,3.57,3.77,3.79,3.81,3.96,3.68,4.56,4.79,5.18,5.87,7.37,8.01,8.76,9.01];
+  var nhlRev = [1.875,1.996,2.083,,2.178,2.44,2.75,2.82,2.93,3.09,3.37,2.63,3.7,3.98,4.1,4.43,4.86,5.09,4.5265];
+  var mlbRev = [3.62,3.88,4.27,4.73,5.11,5.48,5.82,5.9,6.14,6.36,6.81,7.1,7.86,8.39,9.03,9.46,9.9,10.37,10.15377];
 
 
   var ctx = document.getElementById("myChartTwo");
@@ -49,6 +49,15 @@ window.onload = function() {
       ]
     },
     options: {
+      responsive: true,
+      title: {
+        display: true,
+        // padding: -2,
+        fontColor: '#000',
+        fontSize: 8,
+        position: 'bottom',
+        text: ["Average trend in revenue increase year-over-year:","NFL ($636,000)","MLB ($293,770)","NBA ($305,000)","NHL: ($176,500)","Expected loss based on league's previous lockout years:","NFL (estimated revenue from outside source)","MLB (Average of NBA and NHL lockouts $510,000)","NBA ($280,000)","NHL ($740,000)",""]
+      },
       legend: {
         display: true,
         labels: {
@@ -62,7 +71,7 @@ window.onload = function() {
                       fontSize: 14,
                       fontColor: '#000',
                       fontStyle: 'bold',
-                      stepSize: 1,
+                      stepSize: 3,
                       // Include a dollar sign in the ticks
                       callback: function(value, index, values) {
                           return '$' + value;
